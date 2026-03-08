fbeafb846bc96d1a29a2843dc0f6c5d00c78f4e9
zuoshiyue <yokenuoli@vip.qq.com>
2026-03-08 17:27:37 +0800

    fix: 强制刷新缓存 + 角色中文显示

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
     base: '/azur-lane-simulator/',
     build: {
         outDir: 'dist',
-        sourcemap: true
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].[hash].js`,
+                chunkFileNames: `assets/[name].[hash].js`,
+                assetFileNames: `assets/[name].[hash].[ext]`
+            }
+        }
     }
 });
